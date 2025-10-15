4496c51e5b46ca22524fe0a2292b681a4e3409bf
Sameerali0 <sameer.ali.professional@gmail.com>
2025-10-15 23:53:42 +0500

    Add rotating beam animation to radar

--- script.js
+++ script.js
@@ -18,14 +18,38 @@ function drawRadar() {
     ctx.arc(cx, cy, radius, 0, Math.PI * 2);
     ctx.fillStyle = "#0f3d3e";
     ctx.fill();
-    
+
     ctx.linetWidth = 2;
     ctx.strokeStyle = "#00ffcc33"
     ctx.stroke();
 }
 
+let angle = 0;
+
+function drawline() {
+    const endX = cx + radius * Math.cos(angle);
+    const endY = cy + radius * Math.sin(angle);
+
+    const gradient = ctx.createLinearGradient(cx, cy, endX, endY);
+    gradient.addColorStop(0, "rgba(0, 255, 204, 0.2)");
+    gradient.addColorStop(1, "rgba(0, 255, 204, 1)");
+
+    ctx.beginPath()
+    ctx.moveTo(cx, cy)
+    ctx.lineTo(endX, endY);
+    ctx.strokeStyle = gradient;
+    ctx.linetWidth = 3;
+    ctx.stroke();
+}
+
 function animate() {
     drawRadar();
+    drawline();
+
+    angle += 0.03;
+    if (angle > Math.PI * 2) {
+        angle = 0;
+    }
     requestAnimationFrame(animate);
 }
 
