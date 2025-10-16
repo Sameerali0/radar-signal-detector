40389726c37cbc141479624e594801c7ef6355b2
Sameerali0 <sameer.ali.professional@gmail.com>
2025-10-16 12:13:08 +0500

    Add glow effect to radar

--- script.js
+++ script.js
@@ -11,16 +11,17 @@ const cx = w / 2;
 const cy = h / 2;
 const radius = w / 2 * 0.9;
 
-function drawRadar() {
-    ctx.clearRect(0, 0, w, h);
+
+
+function drawRadar() { 
+    ctx.fillStyle = "rgba(7, 18, 31, 0.15)";
+    ctx.fillRect(0, 0, w, h);
 
     ctx.beginPath();
     ctx.arc(cx, cy, radius, 0, Math.PI * 2);
-    ctx.fillStyle = "#0f3d3e";
-    ctx.fill();
-
-    ctx.linetWidth = 2;
-    ctx.strokeStyle = "#00ffcc33"
+    
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#00ffcc33";
     ctx.stroke();
 
     for (let i  = 1; i <= 3; i++) {
@@ -79,7 +80,10 @@ function animate() {
     drawLine();
     drawDots();
     angle += 0.03;
-    
+    if (angle > Math.PI * 2) {
+        angle = 0;
+    }
+
     requestAnimationFrame(animate);
 }
 
